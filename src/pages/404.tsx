a0a82be61b6319d96391cfecbdb2e0139aad9045
Harry Solovay <harrysolovay@gmail.com>
2020-10-05 18:38:11 -0700

    chore: style the 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import styleClassNames from "./404.module.scss";
 import {useRouter} from "next/router";
 
 export default function FourOFour(): React.ReactElement {
   const {asPath} = useRouter();
 
   return (
-    <div>
+    <div className={styleClassNames.container}>
       <h1>404 Not Found</h1>
       <p>
         The route <code>{asPath}</code> does not exist.
